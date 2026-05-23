bbfe7275d90f499ebc76b58bc0fa7f888dd6513f
Dmitry Osten Saken <dmitry@gmail.com>
2026-05-23 12:12:51 +0300

    Add hero image

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-const contactButton = document.getElementById("contactButton");
-
-contactButton.addEventListener("click", function () {
-  alert("Спасибо за интерес! Здесь позже будет форма связи.");
-});
+console.log("Сайт загружен успешно.");
