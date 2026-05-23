79cad8a1913d5b3a6cc9f9a9d229c5e97727f794
Dmitry Osten Saken <dmitry@gmail.com>
2026-05-23 11:32:43 +0300

    First website version

---
+++ script.js
@@ -0,0 +1,5 @@
+const contactButton = document.getElementById("contactButton");
+
+contactButton.addEventListener("click", function () {
+  alert("Спасибо за интерес! Здесь позже будет форма связи.");
+});
